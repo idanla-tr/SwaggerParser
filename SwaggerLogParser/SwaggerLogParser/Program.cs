7fe45f96778a0e398725b79db6b43b72be482d18
idanla-tr <idanla@etoro.com>
2022-02-16 12:59:30 +0200

    Started working on JSON parsing

---
+++ SwaggerLogParser/SwaggerLogParser/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SwaggerLogParser
+{
+    class Program
+    {
+        const string FILENAME = "";
+        static void Main(string[] args)
+        {
+            setFileName();
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void setFileName()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ReadLog()
+        {
+            using (StreamReader r = new StreamReader(FILENAME))
+            {
+                string json = r.ReadToEnd();
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+        }
+
+        public class Item
+        {
+            public string UserData;
+            public string LogicLog;
+            public string OpenPositionSettings;
+        }
+    }
+}
